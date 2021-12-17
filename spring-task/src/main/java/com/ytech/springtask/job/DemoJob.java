33ffc16fab7c271291b8188c051045b2cf9844a1
yuyatech <yuyaapp@163.com>
2021-12-17 16:02:50 +0800

    update demo

--- spring-task/src/main/java/com/ytech/springtask/job/DemoJob.java
+++ spring-task/src/main/java/com/ytech/springtask/job/DemoJob.java
@@ -1,10 +1,14 @@
 package com.ytech.springtask.job;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
+//@Configuration // 1.主要用于标记配置类，兼备Component的效果。
+@EnableScheduling // 2.开启定时任务
 public class DemoJob {
     private final AtomicInteger counts = new AtomicInteger();
 
